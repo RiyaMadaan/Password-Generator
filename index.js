95178075af974219b82f7ee7aab7e7f6f025b6c1
RiyaMadaan <riyamadaan404@gmail.com>
2021-05-27 15:59:04 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,15 @@
+const characters = 'QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkmjnhbgvfcdxsaz1234567890@#$%&';
+
+function generatePass(){
+    var password = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 9; i++ ) {
+        password += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return password;
+}
+
+function myFunction() {
+    document.getElementById("para").innerHTML = generatePass();
+  }
+
